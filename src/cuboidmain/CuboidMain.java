c688023007276bd3080f6f3aadd86291ca9bb979
Blake Vance <nsdnmplz@gmail.com>
2017-10-05 12:58:53 -0400

    Merge origin/master
    Conflicts:
    	README.md

--- src/cuboidmain/CuboidMain.java
+++ src/cuboidmain/CuboidMain.java
@@ -17,11 +17,18 @@ public class CuboidMain {
     public static void main(String[] args) {
         //Calls the methods of the cuboid
         Cuboid<Number> c1 = new Cuboid<>();
+        Cuboid<Number> c2 = new Cuboid<>();
         c1.setBreadth(5);
         c1.setHeight(5);
         c1.setLength(5);
+        c2.setBreadth(.10);
+        c2.setHeight(.10);
+        c2.setLength(.10);
         System.out.println(c1.toString());
-        c1.getVolume();
+
+
+        System.out.println(c2.toString());        
+        
     }
     
 }
